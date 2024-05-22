e3f4d0009236a9a4d2a303f2fcc4216670ac1a32
Inneke04 <innekeindah99@gmail.com>
2024-05-23 07:13:50 +0800

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,77 +1,40 @@
-<?php 
-    include "database/connection.php";
+<?php
+if (isset($_GET["page"])){
+    $page = $_GET["page"];
+} else {
+    $page ="";
+}
+
+switch ($page){
+    case"":
+    case "dashboard":
+        include "pages/dashboard.php";
+        break;
+    case "bagian":
+        include "pages/bagian/bagian.php";
+        break;
+    case "bagiantambah":
+        include "pages/bagian/bagiantambah.php";
+        break;
+    case "bagianhapus":
+        include "pages/bagian/bagianhapus.php";
+        break;
+    case "bagianubah":
+        include "pages/bagian/bagianubah.php";
+        break;
+    case "karyawan":
+        include "pages/karyawan/karyawan.php";
+        break;
+    case "karyawantambah":
+        include "pages/karyawan/karyawantambah.php";
+        break;
+    case "karyawanhapus":
+        include "pages/karyawan/karyawanhapus.php";
+        break;
+    case "karyawanubah":
+        include "pages/karyawan/karyawanubah.php";
+        break;
+    default:
+        include "pages/404.php";
+}
 ?>
-<div class="container-fluid px-4">
-    <div class="row">
-        <div class="col">
-            <h3>Bagian</h3>
-        </div>
-        <div class="col">
-            <a href="?page=bagiantambah" class="btn btn-success float-end">
-                <i class="fa fa-plus-circle"></i>
-                Tambah</a>
-            </div>
-    </div>
-    <div class="row mt-3">
-        <div class="col">
-            <?php 
-            $selectSQL = "SELECT * FROM bagian";
-            $result = mysqli_query($connection,$selectSQL);
-            if (!$result) {
-            ?>
-            <div class="alert alert-danger" role="alert">
-                <?php echo mysqli_error($connection) ?>
-            </div>
-        <?php 
-        return;
-            }
-            if (mysqli_num_rows($result)==0) {
-                ?>
-                <div class="alert alert-light" role="alert">
-                    Data Kosong
-                </div>
-                <?php 
-                return;
-            }
-            ?>
-        
-            <table class="table bg-white rounded shadow-sm  table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col" width="50">#</th>
-                        <th scope="col">Bagian</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php 
-                    $no = 1;
-                    while ($row = mysqli_fetch_assoc($result)) {        
-                   ?>
-                   <tr class="align-middle">
-                    <td>
-                        <?php echo $no++ ?>
-                    </td>
-                    <td>
-                        <?php echo $row["nama"] ?>
-                    </td>
-                    <td>
-                        <a href="#" class="btn btn-primary">
-                        <a href="?page=bagianubah&id=<?php echo $row['id'] ?>" class="btn btn-primary">
-                            <i class="fa fa-edit"></i>
-                            Ubah
-                        </a>
-                        <a href="?page=bagianhapus&id= <?php echo $row['id'] ?>" onclick="javascript: return config('Konfirmasi data akan dihapus?');" class="btn btn-danger">
-                        <a href="?page=bagianhapus&id=<?php echo $row['id'] ?>" onclick="javascript: return config('Konfirmasi data akan dihapus?');" class="btn btn-danger">
-                            <i class="fa fa-trash"></i>
-                            Hapus
-                        </a>
-                    </td>
-                   </tr>
-                    <?php 
-                    } 
-                    ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-</div>
